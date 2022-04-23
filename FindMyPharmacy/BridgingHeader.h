c54b8dd0846d4a987d27784fdd1596296d44e62f
sj-lee98 <denny00123@naver.com>
2022-04-23 14:09:06 +0900

    [sdk 설정 완료]

---
+++ FindMyPharmacy/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  FindMyPharmacy
+//
+//  Created by 이성주 on 2022/04/23.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* BridgingHeader_h */
